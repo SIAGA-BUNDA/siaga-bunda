16f76d34f40c15c983cbd397262ee3e936db6307
imxey <xeylaxeyla@gmail.com>
2024-11-01 23:47:42 +0700

    create controller

---
+++ app/views/home/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Siaga Bunda</title>
+    <link rel="icon" href="img/ikon.png" type="image/x-icon">
+    <link rel="stylesheet" href="style/sebelumLogin.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+    <link
+         rel="stylesheet"
+         href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+</head>
+<body>
+
+    <div class="container">
+        <div class="menu">
+            <p>Siaga Bunda</p>
+            <div class="menu-list">
+                <a href="#" class="isiMenu home">Home</a>
+                <a href="#" class="isiMenu">Calc</a>
+                <a href="#" class="isiMenu">Awikwok</a>
+            </div>
+        </div>
+        <div class="login">
+            <div class="loginDaftar">
+                <a href="SignUp.html">
+                    <button id="signup">
+                        Daftar Sekarang
+                    </button>
+                </a>
+                <a href="signIn.html">
+                    <button id="login">
+                        Login
+                    </button>
+                </a>
+            </div>
+        </div>
+        <div class="content">
+            <div class="content1">
+                <img src="img/ikon.png" alt="">
+                <span>
+                    <h1>Selamat Datang di Siaga Bunda</h1>
+                    <p>Platform yang dirancang khusus untuk mendukung kesehatan para ibu hamil. Kami memahami bahwa masa kehamilan adalah perjalanan yang istimewa, namun juga penuh tantangan.</p>
+                </span>
+                
+            </div>
+            <br><br><br>
+            <div class="content2">
+                <img src="img/ibuBayi.png" width="100%" alt="">
+            </div>
+            <div class="content3">
+                <p class="content3">Jelajahi setiap fitur pada Siaga Bunda</p>
+            </div>
+            <div class="content3">
+                <a href="SignUp.html">
+                    <h2 class="content3">Jelajahi Sekarang</h2>
+                </a>
+                
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
