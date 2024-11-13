5c5cb283f2cd7b859df0db1f26be81fd17f4db84
imxey <xeylaxeyla@gmail.com>
2024-11-13 15:41:19 +0700

    add verif email

--- app/controller/signUp.php
+++ app/controller/signUp.php
@@ -1,15 +1,70 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require '../vendor/autoload.php';
 class signUp extends controller{
     public function index(){
         $data['judul'] = 'Sign Up';
         $data['css'] = 'signUp';
         $data['user'] = $this->model('User_model')->getUsers();
         $this->view('templates/header', $data);
-        $this->view('signUp/index');
+        $this->view('signUp/index', $data);
         $this->view('templates/footer');
     }
     public function tambah(){
-        $this->model('User_model')->tambahUser($_POST);
-        header('Location: '. BASEURL. '/signUp');
+        $email = $_POST['email'];
+        $nama = $_POST['nama'];
+        $token = md5($email.date('Y-m-d H:i:s'));
+        //email
+        $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_OFF;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = 'cv.siaga.bunda@gmail.com';                     //SMTP username
+            $mail->Password   = 'smhx iibz odun vxap';                               //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+            //Recipients
+            $mail->setFrom('SIAGA-BUNDA@example.com', 'Verifikasi Akun Siaga Bunda');
+            $mail->addAddress($email, $nama);     //Add a recipient
+            // $mail->addAddress('ellen@example.com');               //Name is optional
+            // $mail->addReplyTo('info@example.com', 'Information');
+            // $mail->addCC('cc@example.com');
+            // $mail->addBCC('bcc@example.com');
+
+            //Attachments
+            // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = 'Verifikasi akun siaga bunda anda';
+            $mail->Body    = 'Hi '. $nama . " Terimakasih telah mendaftar pada website siaga bunda.<br>
+            Silahkan verifikasi akun anda pada link dibawah ini<br>
+            <a href='". BASEURL. "/signUp/verifikasi/". $token. "'>Verifikasi Akun</a>";
+            // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            if($mail->send()){
+                //tambah ke db
+                $this->model('User_model')->tambahUser($_POST, $token);
+                header('Location: '. BASEURL. '/signIn');
+            }
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    }
+    public function verifikasi($token){
+        $dbToken = $this->model('User_model')->getIdByToken($token);
+        $this->model('User_model')->updateRegis($dbToken);
+        header('Location: '. BASEURL. '/signIn');
     }
 }
--- app/model/user_model.php
+++ app/model/user_model.php
@@ -10,20 +10,35 @@ class user_model {
             $this->db->query($query);
             return $this->db->resultSet();
     }
-    public function tambahUser($data){
+    public function getIdByToken($token) {
+        $query = "select IDUSER from " . $this->table . " where TOKEN = :token";
+        $this->db->query($query);
+        $this->db->bind('token', $token);
+        return $this->db->single()['IDUSER'];
+}
+    public function updateRegis($id){
+        $query = "UPDATE ". $this->table. " SET is_register = '1' WHERE iduser = :id";
+        $this->db->query($query);
+        $this->db->bind('id', $id);
+        $this->db->execute();
+    }
+    public function tambahUser($data, $token){
         $nama = $data['nama'];
         $password = $data['password'];
         $hashed_password = password_hash($password, PASSWORD_BCRYPT);
         $no_telepon = $data['no_telepon'];
         $tanggal_lahir = $data['tanggal_lahir'];
+        $email = $data['email'];
         $lmp = $data['lmp'];
-        $query = "INSERT INTO " . $this->table . " (NAMA, PASSWORD, no_telepon, TANGGALLAHIR, LMP) VALUES (:nama, :password, :no_telepon, TO_DATE(:tanggal_lahir, 'YYYY-MM-DD'), TO_DATE(:lmp, 'YYYY-MM-DD'))";
+        $query = "INSERT INTO " . $this->table . " (NAMA, PASSWORD, no_telepon, TANGGALLAHIR, LMP, EMAIL, is_register, token) VALUES (:nama, :password, :no_telepon, TO_DATE(:tanggal_lahir, 'YYYY-MM-DD'), TO_DATE(:lmp, 'YYYY-MM-DD'), :email, '0', :token)";
         $this->db->query($query);
         $this->db->bind('nama', $nama);
         $this->db->bind('password', $hashed_password);
         $this->db->bind('no_telepon', $no_telepon);
         $this->db->bind('tanggal_lahir', $tanggal_lahir);
         $this->db->bind('lmp', $lmp);
+        $this->db->bind('email', $email);
+        $this->db->bind('token', $token);
         $this->db->execute();
     }
     
