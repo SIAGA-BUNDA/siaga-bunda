bc56927121613ba2be39b5245b502bac4316c87e
amzee03 <2003amirhamzah@gmail.com>
2024-12-24 01:48:13 +0700

    revisi UI

--- app/views/komunitas/index.php
+++ app/views/komunitas/index.php
@@ -104,9 +104,9 @@
           <a href="<?= BASEURL . 'komunitas/komentar/' . $postingan_id ?>"
             class="postingan border border-[#D9D9D9] rounded-xl bg-white flex flex-col items-center justify-center p-4 xl:!p-6 flex-grow hover:cursor-pointer hover:bg-gray-100">
             <div class="postinganAtas flex flex-row w-full">
-              <div class="profil flex flex-row gap-3 flex-1 ">
+              <div class="profil flex flex-row gap-3 flex-1">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                  stroke="currentColor" class="rounded-full bg-[#DDE4C4] w-7 h-7">
+                  stroke="currentColor" class="rounded-full bg-gray-400 w-7 h-7">
                   <path stroke-linecap="round" stroke-linejoin="round"
                     d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                 </svg>
